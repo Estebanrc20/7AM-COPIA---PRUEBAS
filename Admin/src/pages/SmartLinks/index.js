9ec77f4e74c56312b5b436a56a96b8a60323b8a2
Estebanrc20 <esteban.rc0120@gmail.com>
2025-08-06 15:28:51 -0500

    se modifica index de planificacion,smarlinks,anuncios

--- Admin/src/pages/SmartLinks/index.js
+++ Admin/src/pages/SmartLinks/index.js
@@ -63,9 +63,9 @@ const Home = () => {
           textAlign: "center",
           padding: "2rem"
         }}>
-          <p style={{ fontSize: "1.2rem", color: "#363636ff" }}>
+          <h4 style={{ fontSize: "1.2rem", color: "#363636ff" }}>
             No se encontró un iframe configurado para esta sección.
-          </p>
+          </h4>
         </div>
       ) : (
         <iframe
