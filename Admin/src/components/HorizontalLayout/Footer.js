e2903e1299c746335c1ed28d5fc8530c01caa8ce
Estebanrc20 <esteban.rc0120@gmail.com>
2025-08-27 18:05:14 -0500

    se modifica scss/custom/structure/footer.scss

--- Admin/src/components/HorizontalLayout/Footer.js
+++ Admin/src/components/HorizontalLayout/Footer.js
@@ -1,24 +1,20 @@
 import React from "react"
-import { Container, Row } from "reactstrap"
+import { Container, Row, Col } from "reactstrap"
 
 const Footer = () => {
   return (
-    <footer 
-      className="footer text-center text-muted" 
-      style={{ fontSize: "12px", padding: "5px 0" }}
-    >
-      <Container fluid>
-        <Row>
-          <div className="col-12">
-            © {new Date().getFullYear()} 7AM Digital
-            <span className="d-none d-sm-inline-block">
-              {" "}
-              <i className="mdi mdi-heart text-danger"></i>
-            </span>
-          </div>
-        </Row>
-      </Container>
-    </footer>
+    <React.Fragment>
+      <footer className="footer">
+        <Container fluid={true}>
+          <Row>
+            <div className="col-12">
+              © {new Date().getFullYear()} 7AM Digital<span className="d-none d-sm-inline-block"> 
+              {" "}<i className="mdi mdi-heart text-danger"></i></span>
+            </div>
+          </Row>
+        </Container>
+      </footer>
+    </React.Fragment>
   )
 }
 
