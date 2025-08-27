b4e563cbb34a4610be0643eb1fdaaa20c69c8860
Estebanrc20 <esteban.rc0120@gmail.com>
2025-08-27 17:55:34 -0500

    se modifica HorizontalLayout/footer.js

--- Admin/src/components/HorizontalLayout/Footer.js
+++ Admin/src/components/HorizontalLayout/Footer.js
@@ -1,20 +1,24 @@
 import React from "react"
-import { Container, Row, Col } from "reactstrap"
+import { Container, Row } from "reactstrap"
 
 const Footer = () => {
   return (
-    <React.Fragment>
-      <footer className="footer">
-        <Container fluid={true}>
-          <Row>
-            <div className="col-12">
-              © {new Date().getFullYear()} 7AM Digital<span className="d-none d-sm-inline-block"> 
-              {" "}<i className="mdi mdi-heart text-danger"></i></span>
-            </div>
-          </Row>
-        </Container>
-      </footer>
-    </React.Fragment>
+    <footer 
+      className="footer text-center text-muted" 
+      style={{ fontSize: "12px", padding: "5px 0" }}
+    >
+      <Container fluid>
+        <Row>
+          <div className="col-12">
+            © {new Date().getFullYear()} 7AM Digital
+            <span className="d-none d-sm-inline-block">
+              {" "}
+              <i className="mdi mdi-heart text-danger"></i>
+            </span>
+          </div>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
